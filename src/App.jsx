f8bd89fffc45f21dbdab6ec3fbf3c5aa4be90a27
Tanmay Kanase <tanmaykanase06@gmial.com>
2026-06-17 15:17:42 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
 import Internships from "./pages/Internships";
 import Projects from "./pages/Projects";
 import Home from "./pages/Home";
@@ -8,8 +7,6 @@ import Home from "./pages/Home";
 function App() {
   return (
     <>
-      <Navbar />
-
       <Routes>
         <Route path="/internship" element={<Internships />}></Route>
         <Route path="/projects" element={<Projects />} />
